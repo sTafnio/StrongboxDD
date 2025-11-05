2ace34bee68b325b98d3ec3a795e0d890e3d9e76
sTafnio <>
2025-11-05 22:11:24 +0100

    initial version

---
+++ StrongboxDD.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.Shared.Enums;
+using SharpDX;
+
+namespace StrongboxDD;
+
+public class StrongboxDD : BaseSettingsPlugin<StrongboxDDSettings>
+{
+    public override bool Initialise()
+    {
+        return true;
+    }
+    public override Job Tick()
+    {
+        return null;
+    }
+
+    public override void Render()
+    {
+        var detonateBoxes = GameController.EntityListWrapper.ValidEntitiesByType[EntityType.Chest]
+            .Where(x => x.TryGetComponent<ObjectMagicProperties>(out var omp) &&
+                    omp.Mods.Contains("ChestExplodeCorpses"));
+                    
+        foreach (var box in detonateBoxes)
+        {
+            var pos = box.PosNum;
+            Graphics.DrawFilledCircleInWorld(pos, Settings.Radius, Color.Red);
+        }
+    }
+}
---
+++ StrongboxDDSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace StrongboxDD;
+
+public class StrongboxDDSettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+    public RangeNode<float> Radius { get; set; } = new(82, 0, 400);
+}
